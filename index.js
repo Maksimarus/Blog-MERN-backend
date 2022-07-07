d07f65653ffa3eb7e3697a327aded04ade57bce8
Maksim Chuchuk <cucukmaksim@gmail.com>
2022-07-07 16:18:15 +0300

    feat: добавить авторизацию

--- index.js
+++ index.js
@@ -18,6 +18,44 @@ const PORT = 3333;
 const app = express();
 app.use(express.json());
 
+app.post('/auth/login', async (req, res) => {
+  try {
+    const user = await UserSchema.findOne({email: req.body.email});
+    if (!user) {
+      return res.status(404).json({
+        message: 'Пользователь не найден',
+      });
+    }
+    const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+    if (!isValidPass) {
+      return res.status(404).json({
+        message: 'Неверный логин или пароль',
+      });
+    }
+    const {passwordHash, ...userData} = user._doc;
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret123',
+      {
+        expiresIn: '30d',
+      },
+    );
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Не удалось авторизоваться',
+    });
+  }
+});
+
 app.post('/auth/register', registerValidation, async (req, res) => {
   try {
     const errors = validationResult(req);
